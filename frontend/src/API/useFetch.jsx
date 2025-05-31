4aa133b28d2182331ee7c0ac54d5ccc073c33e39
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-05-31 10:41:37 +0200

    Add useFetch hook to be reused by the fetch functions

---
+++ frontend/src/API/useFetch.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url, method, body = null) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            const token = localStorage.getItem('token');
+
+            const headers = {
+                'Authorization': `Bearer ${token}`
+            }
+
+            if (body && ['POST', 'PUT', 'PATCH'].includes(method.toUpperCase())) {
+                headers['Content-Type'] = 'application/json';
+            }
+
+            try {
+                const response = await fetch(url, {
+                    method: method,
+                    headers: headers,
+                    body: body ? JSON.stringify(body) : null
+                });
+
+                if (response.ok) {
+                    const json = await response.json();
+                    setData(json);
+                } else {
+                    throw new Error('HTTP error! Status: ', response.status);
+                }
+            } catch (err) {
+                setError(err.message || 'Unknown error')
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [url, method, body]);
+
+    return { data, error, loading };
+};
+
+export default useFetch
